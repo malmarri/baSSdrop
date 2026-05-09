21d0406f8b0ff387aa5dc9bb8dc80e808008efc6
Mohamed AlMarri <mohamedalmarri@Ms-Pro.local>
2026-05-09 14:55:29 +0400

    feat: add asymmetrical distance parameters for end-specific recalibration

--- baSSdrop.cpp
+++ baSSdrop.cpp
@@ -11,17 +11,22 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // Check if the SNP file is provided as an argument
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <path_to_SNP_file>\n";
+    // Check if the SNP file and the two end-distances are provided
+    if (argc < 4) {
+        cerr << "Usage: " << argv[0] << " <path_to_SNP_file> <bases_from_start> <bases_from_end>\n";
+        cerr << "Example: " << argv[0] << " known_snps.txt 5 2\n";
+        cerr << "  (Recalibrates the first 5 bases and the last 2 bases of the read string)\n";
         return 1;
     }
+
+    int bases_from_start = stoi(argv[2]);
+    int bases_from_end = stoi(argv[3]);
 
     // Use fast IO
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    unordered_set<string> snp_positions;  // To store SNPs for fast lookups
+    unordered_set<string> snp_positions;
 
     ifstream snpFile(argv[1]);
     if (!snpFile.is_open()) {
@@ -36,6 +41,7 @@ int main(int argc, char* argv[]) {
 
         string chrom = cols[0];
         int pos = stoi(cols[1]);
+        // Target C/T transitions for forward strands, G/A for reverse strands
         string snp = (cols[2] == "C" && cols[3] == "T") || (cols[2] == "T" && cols[3] == "C") ? "CT" : 
                      (cols[2] == "G" && cols[3] == "A") || (cols[2] == "A" && cols[3] == "G") ? "GA" : "";
 
@@ -45,8 +51,8 @@ int main(int argc, char* argv[]) {
     }
 
     snpFile.close();
-
-    cerr << "SNP list loaded\n";
+    cerr << "SNP list loaded. Restricting recalibration to the first " << bases_from_start 
+         << " bases and the last " << bases_from_end << " bases of the read.\n";
 
     vector<string> cols;
     string qual, newQual;
@@ -69,11 +75,15 @@ int main(int argc, char* argv[]) {
         bool overlap_found = false;
 
         for (int i = 0; i < read.length(); i++) {
-            int currentPos = position + i;
-            string snpType = (reverse) ? "GA" : "CT";
-            if (snp_positions.find(chrom + "_" + to_string(currentPos) + "_" + snpType) != snp_positions.end()) {
-                overlap_found = true;
-                newQual[i] = '!';
+            // ONLY recalibrate if within the specified distance from the start or end of the BAM read string
+            if (i < bases_from_start || i >= (read.length() - bases_from_end)) {
+                int currentPos = position + i; // Assumes perfect match without indels/clipping
+                string snpType = (reverse) ? "GA" : "CT";
+                
+                if (snp_positions.find(chrom + "_" + to_string(currentPos) + "_" + snpType) != snp_positions.end()) {
+                    overlap_found = true;
+                    newQual[i] = '!'; // Downgrade to Phred 0
+                }
             }
         }
 
@@ -81,8 +91,8 @@ int main(int argc, char* argv[]) {
             cols[10] = newQual;
         }
 
-        for (const auto& col : cols) {
-            cout << col << '\t';
+        for (int i = 0; i < cols.size(); i++) {
+            cout << cols[i] << (i == cols.size() - 1 ? "" : "\t");
         }
         cout << '\n';
     }
